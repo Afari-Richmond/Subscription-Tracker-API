c9040c233a9183ec90076c8ae1a304980d03847a
richmondafari <afaririchmond1000@gmail.com>
2025-04-29 17:58:14 +0000

    Created the User Routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,25 @@
 import { Router } from "express";
 
-const userRoute = Router();
+const userRouter = Router();
 
-userRoute.
+userRouter.get("/", (req, res) => {
+  res.send("GET all Users");
+});
+
+userRouter.get("/:id", (req, res) => {
+  res.send("GET a single user");
+});
+
+userRouter.post("/", (req, res) => {
+  res.send("CREATE a new user");
+});
+
+userRouter.put("/:id", (req, res) => {
+  res.send("UPDATE user");
+});
+
+userRouter.delete("/:id", (req, res) => {
+  res.send("DELETE user");
+});
+
+export default userRouter;
